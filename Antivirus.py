9c6cf6b774438b2776e1ba88f0218b44a1208c15
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-11-09 18:12:20 -0500

    Finished antivirus

--- Antivirus.py
+++ Antivirus.py
@@ -1,5 +1,6 @@
 import os, mmap
 from mmap import mmap
+import re
 
 print("System is starting...\n")
 
@@ -10,42 +11,90 @@ VDfile.close()
 print("Virus definitions loaded!\n")
 
 def scan(file):
-   print(file)
-   infections = 0
-   for i,vd in enumerate(VirusDefinitions):
-      with open(cwdf, "r+b") as candidate:
-         mm = mmap(candidate.fileno(), 0)
-         offset = mm.find(vd)
-         if offset>=0:
-            infections += 1
-            neutralize(file, offset)
-         mm.close()
-   return infections
+    # print(file)
+    infections = 0
+    for i,vd in enumerate(VirusDefinitions):
+        # print (vd)
+        vd = vd[0:-1]
+        with open(cwdf, "r+b") as candidate:
+            # print ("file: {}".format(candidate.fileno()))
+            mm = mmap(candidate.fileno(), 0)
+            # print("Candidate: {}".format(candidate))
+            # print ("vd: {}".format(vd))
+            # print (mm.readline())
+            offset = mm.find(vd)
+            # print ("offset {}".format(offset))
+            if offset>=0:
+                infections += 1
+                neutralize(file, offset)
+                mm.close()
 
-def quarantine(virus, vd):
-   print('')
-   os.rename(virus, )
+    if infections > 0:
+        quarantine(file)
+    return infections
+
+def quarantine(virus):
+    folder_name = 'quarantine'
+    file_name = get_file_name(virus)
+    destination = '{}/{}'.format(folder_name, file_name)
+    if os.path.isdir(folder_name):
+        move_file(virus, destination)
+    else:
+        os.makedirs(folder_name)
+        move_file(virus, destination)
+
+
+def move_file(cur_dest, new_dest):
+    print ('Quarantining {}'.format(cur_dest))
+    os.rename(cur_dest, new_dest)
+
+
+def get_file_name(file):
+    return re.search('\/.+\..+', file).group(0)[1:]
 
 def neutralize(virus, offset):
-   X = bytes('x'*8, 'utf-8')
-   overwrite(virus, X, offset)
+    X = bytes('x'*8, 'utf-8')
+    overwrite(virus, X, offset)
 
 def overwrite(filename, s, pos):
-   f = open(filename, 'r+')
-   m = mmap(f.fileno(), os.path.getsize(filename))
-   origSize = m.size()
+    f = open(filename, 'r+')
+    m = mmap(f.fileno(), os.path.getsize(filename))
+    origSize = m.size()
 
-   m[pos:(pos+len(s))] = s
-   m.close()
-   f.close()
+    m[pos:(pos+len(s))] = s
+    m.close()
+    f.close()
 
 directory = 'Programs'
 
+# print (os.listdir(directory))
+ESC_CHAR = '\033'
+COLOR_GREEN = '32m'
+COLOR_RED = '5;31m'
+RESET = '[0m'
 for cf in os.listdir(directory):
-   cwdf = directory + '/' + cf
-   infections = scan(cwdf)
-   if infections>0:
-      # quarantine(cf)
-      print('>Found {} infections! File quarantined'.format(infections))
-   else:
-      print('>The file is clean!')
+    cwdf = directory + '/' + cf
+    print ("\n=================================")
+    print ("Scanning {}".format(cf))
+    print ("=================================")
+    infections = scan(cwdf)
+    # print ( infections )
+    if infections>0:
+        # quarantine(cf)
+        print('{} {}[{}Found {} infections in {}! File quarantined{}{}'.format(
+            '☠️',
+            ESC_CHAR,
+            COLOR_RED,
+            infections,
+            cf,
+            ESC_CHAR,
+            RESET
+        ))
+    else:
+        print('{}[{}{} is clean!{}{}'.format(
+            ESC_CHAR,
+            COLOR_GREEN,
+            cf,
+            ESC_CHAR,
+            RESET
+        ))
